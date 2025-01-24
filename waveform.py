bab8bbd79853199e15ad70c92e5eac368ef0fb2f
playerpiano05 <salvation0169@gmail.com>
2025-01-24 15:00:34 -0500

    updated waveform.py

--- waveform.py
+++ waveform.py
@@ -1,11 +1,10 @@
 from manim import *
 import numpy as np
 
-class TaperedSpiral3D(ThreeDScene):
+class TaperedSpiral(VGroup):
     def __init__(
         self,
-        orientation=(75 * DEGREES, 30 * DEGREES),
-        distance=6,
+        orientation=(0, 0, 0),  
         position=ORIGIN,
         show_axes=True,
         speed=1.0,
@@ -14,12 +13,15 @@ class TaperedSpiral3D(ThreeDScene):
         r_max=0.4,
         sigma=0.3,
         x_span=1.0,
-        formula=None,
+        formula=None, 
+        color=BLUE,    
+        particle_color=RED,
         **kwargs
     ):
+      
         super().__init__(**kwargs)
+
         self.orientation = orientation
-        self.distance = distance
         self.position = position
         self.show_axes = show_axes
         self.speed = speed
@@ -28,61 +30,65 @@ class TaperedSpiral3D(ThreeDScene):
         self.r_max = r_max
         self.sigma = sigma
         self.x_span = x_span
-        self.formula = formula
+        self.color = color
+        self.particle_color = particle_color
 
-    def construct(self):
-        phi, theta = self.orientation
-        self.set_camera_orientation(phi=phi, theta=theta, distance=self.distance)
-        if self.show_axes:
-            axes = ThreeDAxes(
-                x_range=[-1, 1, 0.5],
-                y_range=[-1, 1, 0.5],
-                z_range=[-1, 1, 0.5],
-                x_length=4, y_length=4, z_length=4,
-            )
-            axes.move_to(self.position)
-            self.add(axes)
-
-        def default_wave_packet_func(t):
+        def default_wave_packet(t):
             x_val = t * self.x_span
-            radius = self.r_max * np.exp(-(t ** 2) / (self.sigma ** 2))
+            radius = self.r_max * np.exp(-(t**2) / (self.sigma**2))
             angle = 2 * np.pi * self.turns * self.frequency * t
             y_val = radius * np.cos(angle)
             z_val = radius * np.sin(angle)
             return np.array([x_val, y_val, z_val])
+        
+        self.wave_packet_func = formula if formula else default_wave_packet
 
-        wave_packet_func = self.formula if self.formula else default_wave_packet_func
-        spiral = ParametricFunction(
-            wave_packet_func,
+        self.t_tracker = ValueTracker(-1)
+
+        self.spiral = ParametricFunction(
+            self.wave_packet_func,
             t_range=[-1, -1],
-            color=BLUE,
+            color=self.color,
         )
-        spiral.shift(self.position)
 
-        particle = Sphere(radius=0.05, color=RED)
-        particle.move_to(wave_packet_func(-1) + self.position)
-
-        t_tracker = ValueTracker(-1)
+        self.particle = Sphere(radius=0.05, color=self.particle_color)
+        self.particle.move_to(self.wave_packet_func(-1))
 
         def spiral_updater(mob):
-            current_t = t_tracker.get_value()
+            current_t = self.t_tracker.get_value()
             new_spiral = ParametricFunction(
-                wave_packet_func,
+                self.wave_packet_func,
                 t_range=[-1, current_t],
-                color=BLUE
+                color=self.color
             )
-            new_spiral.shift(self.position)
             mob.become(new_spiral)
 
-        spiral.add_updater(spiral_updater)
+        self.spiral.add_updater(spiral_updater)
 
         def particle_updater(mob):
-            current_t = t_tracker.get_value()
-            mob.move_to(wave_packet_func(current_t) + self.position)
+            current_t = self.t_tracker.get_value()
+            mob.move_to(self.wave_packet_func(current_t))
 
-        particle.add_updater(particle_updater)
+        self.particle.add_updater(particle_updater)
 
-        self.add(spiral, particle)
-        run_time_value = 5.0 / self.speed
-        self.play(t_tracker.animate.set_value(1), run_time=run_time_value, rate_func=smooth)
-        self.wait()
+        if self.show_axes:
+            self.axes = ThreeDAxes(
+                x_range=[-1, 1, 0.5],
+                y_range=[-1, 1, 0.5],
+                z_range=[-1, 1, 0.5],
+                x_length=4,
+                y_length=4,
+                z_length=4,
+            )
+            self.add(self.axes)
+        self.add(self.spiral, self.particle)
+        self.shift(self.position)
+        x_angle, y_angle, z_angle = self.orientation
+        self.rotate(x_angle * DEGREES, axis=RIGHT, about_point=ORIGIN)
+        self.rotate(y_angle * DEGREES, axis=UP, about_point=ORIGIN)
+        self.rotate(z_angle * DEGREES, axis=OUT, about_point=ORIGIN)
+
+    def get_spiral_animation(self, run_time=None, rate_func=smooth):
+        if run_time is None:
+            run_time = 5.0 / self.speed
+        return self.t_tracker.animate.set_value(1).set_run_time(run_time).set_rate_func(rate_func)
